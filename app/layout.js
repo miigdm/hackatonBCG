bea380452f7a8856c2ad5c5bbee7b7691b967f35
Manuel Alba <manuel.alba@uchile.cl>
2023-10-07 13:25:07 -0300

    fix logut

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,9 @@ export default function RootLayout({ children }) {
   };
 
   const handleClose = () => {
-      
-    
+      localStorage.clear()
+      window.location.href = "/"
+
   };
 
   useEffect(() => {
